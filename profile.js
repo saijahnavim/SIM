c64aac1eb74426387fe3534322ec91118524012d
saijahnavim <65816422+saijahnavim@users.noreply.github.com>
2020-05-26 22:52:36 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -103,6 +103,6 @@ function signout(){
     firebase.auth().signOut().then(function() {
         window.location.href = "index.html";
       }).catch(function(error) {
-        // An error happened.
+        // An error happened
       });
 }
